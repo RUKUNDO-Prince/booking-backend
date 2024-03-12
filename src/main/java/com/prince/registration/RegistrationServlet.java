4e9e1aa60119eed841385d84f35f5cf27d51af9a
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-03-12 21:12:52 +0200

    successfully rendered the credentials

--- src/main/java/com/prince/registration/RegistrationServlet.java
+++ src/main/java/com/prince/registration/RegistrationServlet.java
@@ -16,8 +16,16 @@ public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String mobile = request.getParameter("contact");
+		
 		PrintWriter out = response.getWriter();
-		out.print("Working");
+		out.print(name);
+		out.print(email);
+		out.print(password);
+		out.print(mobile);
 	}
 
 }
