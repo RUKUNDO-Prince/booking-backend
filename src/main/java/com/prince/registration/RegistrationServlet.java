8157670449cba8bb8947ac4fa3d4c417233ff4d4
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-03-12 21:54:09 +0200

    successfully added users credentials in the database

--- src/main/java/com/prince/registration/RegistrationServlet.java
+++ src/main/java/com/prince/registration/RegistrationServlet.java
@@ -1,12 +1,17 @@
 package com.prince.registration;
 
-import jakarta.servlet.ServletException;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException; 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Servlet implementation class RegistrationServlet
@@ -18,14 +23,41 @@ public class RegistrationServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
-		String password = request.getParameter("password");
+		String password = request.getParameter("pass");
 		String mobile = request.getParameter("contact");
 		
-		PrintWriter out = response.getWriter();
-		out.print(name);
-		out.print(email);
-		out.print(password);
-		out.print(mobile);
+		RequestDispatcher dispatcher = null;
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "root", "");
+			PreparedStatement pst = con.prepareStatement("INSERT INTO users (name, password, email, mobile) VALUES (?, ?, ?, ?)");
+			pst.setString(1, name);
+			pst.setString(2, password);
+			pst.setString(3, email);
+			pst.setString(4, mobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount > 0) {
+				request.setAttribute("status", "Success");
+			} else {
+				request.setAttribute("status", "Failed");
+			}
+			
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
