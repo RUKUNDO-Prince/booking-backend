7b13de265b6172169b0689b1eede5374d9c78239
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-03-13 08:19:35 +0200

    successfully logged user in

--- src/main/java/com/prince/registration/Login.java
+++ src/main/java/com/prince/registration/Login.java
@@ -1,14 +1,20 @@
 package com.prince.registration;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.cj.Session;
 
 /**
  * Servlet implementation class Login
@@ -21,6 +27,8 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -28,9 +36,20 @@ public class Login extends HttpServlet {
 			PreparedStatement pst = con.prepareStatement("SELECT * FROM users WHERE email = ? AND password = ?");
 			pst.setString(1, email);
 			pst.setString(2, password);
-		} catch (Exception e) {
 			
-		})
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				session.setAttribute("name", rs.getString("name"));
+				dispatcher = request.getRequestDispatcher("index.jsp");
+			} else {
+				request.setAttribute("status", "failed");
+				dispatcher = request.getRequestDispatcher("login.jsp");
+			}
+			
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
